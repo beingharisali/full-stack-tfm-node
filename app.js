1961c3d553a2c8ef93fa94760f7d2c84447b00dc
hussayn-naqvi-567 <hussainshah44332957@gmail.com>
2025-10-30 17:09:54 +0500

    built auth APIs (register/login)using bcrypt

--- app.js
+++ app.js
@@ -1,14 +1,12 @@
 require("dotenv").config();
 const express = require("express");
+const authRoutes = require("./routes/authRoutes");
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.get("/", (req, res) => {
-  res.status(200).json({
-    success: true,
-    msg: "API working",
-  });
-});
+app.use(express.json());
+
+app.use("/api/auth", authRoutes);
 
 app.listen(port, () => {
   console.log(`Application is up and running on port ${port}`);
