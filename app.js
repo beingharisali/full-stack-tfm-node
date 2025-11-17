a53f35542e3b71d680b4d95e3755219bd22b6a49
Fatima <bc220417002fzn@vu.edu.pk>
2025-11-17 16:36:16 +0500

    Socket.io

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 require("dotenv").config();
 const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
 const authRoutes = require("./routes/authRoutes");
 const taskRoutes = require("./routes/taskRoutes");
 const app = express();
@@ -11,6 +13,16 @@ const { notFound, globalErrorHandler } = require("./middlewares/errorHandler");
 const port = process.env.PORT || 5000;
 connectdb();
 
+const server = http.createServer(app);
+
+const io = new Server(server, {
+	cors: {
+		origin: process.env.CLIENT_URL || "http://localhost:3000",
+		methods: ["GET", "POST"],
+		credentials: true
+	}
+});
+
 app.use(express.json());
 
 app.use(cors({}));
@@ -21,6 +33,27 @@ app.use(notFound);
 
 app.use(globalErrorHandler);
 
-app.listen(port, () => {
+io.on("connection", (socket) => {
+	console.log("A user connected:", socket.id);
+
+	socket.on("join", (userId) => {
+		socket.join(userId);
+		console.log(`User ${userId} joined room`);
+	});
+
+	socket.on("message", (data) => {
+		console.log("Message received:", data);
+		io.emit("message", data);
+	});
+
+	socket.on("disconnect", () => {
+		console.log("User disconnected:", socket.id);
+	});
+});
+
+app.set("io", io);
+
+server.listen(port, () => {
 	console.log(`Application is up and running on port ${port}`);
+	console.log(`Socket.io server is ready`);
 });
