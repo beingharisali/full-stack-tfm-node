15c8d5aab9487381ccf935c89931ea6fb08dbfff
beingharisali <theharis99@gmail.com>
2025-11-28 16:57:13 +0500

    updated url

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
 	cors: {
-		origin: process.env.CLIENT_URL || "http://localhost:3000"|| "https://full-stack-tfm-next.vercel.app/",
+		origin: process.env.CLIENT_URL || "http://localhost:3000"|| "https://full-stack-tfm-next.vercel.app",
 		methods: ["GET", "POST"],
 		credentials: true
 	}
